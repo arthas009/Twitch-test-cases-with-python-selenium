b3409b9b75f87ce06515178e82c0f2672869af6e
Yusuf <36654247+arthas009@users.noreply.github.com>
2024-06-21 01:10:46 +0300

    Added pytest as test runner

--- tests/test_twitch.py
+++ tests/test_twitch.py
@@ -1,26 +1,23 @@
-import unittest
+import pytest
 from utils.driver_setup import get_driver
 from pages.twitch_home_page import TwitchHomePage
 from pages.twitch_search_page import TwitchSearchPage
 from pages.twitch_streamer_page import TwitchStreamerPage
 
-class TestTwitch(unittest.TestCase):
-    def setUp(self):
-        self.driver = get_driver()
-        self.home_page = TwitchHomePage(self.driver)
-        self.search_page = TwitchSearchPage(self.driver)
-        self.streamer_page = TwitchStreamerPage(self.driver)
+@pytest.fixture(scope="module")
+def driver():
+    driver = get_driver()
+    yield driver
+    driver.quit()
 
-    def test_search_starcraft(self):
-        self.home_page.go_to_home_page()
-        self.home_page.click_search_icon()
-        self.search_page.input_search_query("StarCraft II")
-        self.search_page.scroll_down(2)
-        self.streamer_page.select_streamer()
-        self.streamer_page.take_screenshot("streamer_screenshot.png")
+def test_search_starcraft(driver):
+    home_page = TwitchHomePage(driver)
+    search_page = TwitchSearchPage(driver)
+    streamer_page = TwitchStreamerPage(driver)
 
-    def tearDown(self):
-        self.driver.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+    home_page.go_to_home_page()
+    home_page.click_search_icon()
+    search_page.input_search_query("StarCraft II")
+    search_page.scroll_down(2)
+    streamer_page.select_streamer()
+    streamer_page.take_screenshot("streamer_screenshot.png")
